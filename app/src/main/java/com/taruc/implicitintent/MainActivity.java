1e60d9e84b454dd985385d9bbe4d834376504ab0
mazlinda <mazlindanm@tarc.edu.my>
2018-05-22 15:02:06 +0800

    Lab 2-2

--- app/src/main/java/com/taruc/implicitintent/MainActivity.java
+++ app/src/main/java/com/taruc/implicitintent/MainActivity.java
@@ -1,13 +1,17 @@
 package com.taruc.implicitintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //declare views
     private Button buttonSend, buttonView, buttonCall, buttonHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +22,34 @@ public class MainActivity extends AppCompatActivity {
         buttonView = findViewById(R.id.buttonView);
         buttonHome = findViewById(R.id.buttonHome);
         buttonSend = findViewById(R.id.buttonSend);
-
-        //define function
-
     }
+        //define functions
+        public void viewTarc(View v)
+        {
+            String url = "http://www.tarc.edu.my";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
+
+        public void call(View v)
+        {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "0122274156"));
+            startActivity(intent);
+        }
+
+        public void home(View v)
+        {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            startActivity(intent);
+        }
+
+        public void send(View v)
+        {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:" + "mazlindanm@tarc.edu.my"));
+            startActivity(intent);
+        }
+
+
 }
